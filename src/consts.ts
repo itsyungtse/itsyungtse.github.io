2fdbd22021b57487558d24915d8e22823fe821ae
yungtse <yungtseteng@gmail.com>
2026-01-18 13:44:02 +0800

    refactor: update header component and improve homepage layout; remove unused blog posts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'YungTse Teng';
+export const SITE_TITLE = 'yungtse';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
